78fa4052488dfa7356a5695ae68795a83e1705e7
Denise Architetto <denise@digital-pioneers.com>
2015-05-04 11:33:43 -0400

    Removed underscore and added lodash

--- client/newGameBoard/newGameBoard.js
+++ client/newGameBoard/newGameBoard.js
@@ -6,13 +6,12 @@ Template.newGameBoard.helpers({
 
 Template.newGameBoard.events({
     'submit form': function (e) {
-console.log('hi')
-
 		e.preventDefault();
         var formData = form2js(e.currentTarget);
 
-        console.log(formData);
+		Games.insert({quads:formData});        
 
+        console.log(formData);
 
     }
   });
