49913e48819628d600ef24d64826cd921039b938
Denise Architetto <denise@digital-pioneers.com>
2015-05-04 11:22:25 -0400

    Added fom2js and moved the game submission to the game board

--- client/newGameBoard/newGameBoard.js
+++ client/newGameBoard/newGameBoard.js
@@ -3,3 +3,16 @@ Template.newGameBoard.helpers({
 	    return Quads.find({});
 	}
 });
+
+Template.newGameBoard.events({
+    'submit form': function (e) {
+console.log('hi')
+
+		e.preventDefault();
+        var formData = form2js(e.currentTarget);
+
+        console.log(formData);
+
+
+    }
+  });
