05254e09bfaf7d82ee7e0d2bd954026f4f914506
Denise Architetto <denise@digital-pioneers.com>
2015-05-04 15:13:18 -0400

    set up data for retroSpectIt route, as well as default route.

--- client/newGameBoard/newGameBoard.js
+++ client/newGameBoard/newGameBoard.js
@@ -1,13 +1,8 @@
-Template.newGameBoard.helpers({
-	quads: function(){
-	    return Quads.find({});
-	}
-});
-
 Template.newGameBoard.events({
     'submit form': function (e) {
 		e.preventDefault();
         var formData = _.values(form2js(e.currentTarget));
+
         Games.insert({quads:formData}, function(er, val)
 			{
 				Router.go('retrospectIt', {id: val});
