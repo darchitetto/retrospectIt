9bc8d25ab9a683c1cce4813a7b6c6245791250bb
Denise Architetto <denise@digital-pioneers.com>
2015-05-04 17:30:54 -0400

    ugh, because algorithms

---
+++ client/retrospectIt/retrospectIt.js
@@ -0,0 +1,24 @@
+Template.retrospectIt.helpers({
+	rowCount: function() {
+		var count = Template.currentData().boardQuads.length / 2;
+		console.log(count);
+		return _.range(count);
+	},
+	gridData: function() {
+		var boardQuads = Template.currentData().boardQuads;
+		var rowData = [];
+		var gridData = [];
+
+		for(var n=1; n < boardQuads.length + 1; n++) {
+			rowData.push(boardQuads[n-1]);
+			if(n % 2 == 0) {
+				gridData.push(rowData);
+				rowData = [];
+			}
+		}
+		console.log(gridData);
+		return [];
+	}
+})
+
+  
