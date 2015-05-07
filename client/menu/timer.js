9485f0b61d111903472b0d73e8404507dae6536e
Denise Architetto <denise@digital-pioneers.com>
2015-05-07 11:20:20 -0400

    add in a reset button for the timer and include font-awesome

--- client/menu/timer.js
+++ client/menu/timer.js
@@ -1,36 +1,39 @@
 var clock;
 
-Template.timer.onRendered(
-	function(){
-		clock = $('.retro-timer').FlipClock(300, {
+function configureClock() {
+	clock = $('.retro-timer').FlipClock(300, {
 			clockFace: 'MinuteCounter',
 			autoStart: false,
 			countdown: true,
 			callbacks: {
-				stop: function() {
-					console.log('stop')
-				}
+				reset: configureClock
 			}
 		});
+}
 
-
-	}
-);
+Template.timer.onRendered(configureClock);
 
 
 Template.timer.events({
 	"click .timer":function(event) {
-			var timerButton = $(event.currentTarget);
-			if(timerButton.hasClass('glyphicon-play')) {
-				clock.start();				
-				timerButton.removeClass('glyphicon-play');
-				timerButton.addClass('glyphicon-pause')
-			} else {
-				timerButton.removeClass('glyphicon-pause');
-				timerButton.addClass('glyphicon-play')
-				clock.stop();
+		var timerButton = $(event.currentTarget);
+		if(timerButton.hasClass('fa-play')) {
+			clock.start();				
+			timerButton.removeClass('fa-play');
+			timerButton.addClass('fa-pause');
+		} else {
+			timerButton.removeClass('fa-pause');
+			timerButton.addClass('fa-play');
+			clock.stop();
+		}
+	},
+	"click .timer-reset":function(event) {
+			clock.reset();
+
+			var playPauseButton = $('.timer');
+			if(playPauseButton.hasClass('fa-pause')) {
+				playPauseButton.removeClass('fa-pause');
+				playPauseButton.addClass('fa-play');
 			}
-
-
 	}
 });
